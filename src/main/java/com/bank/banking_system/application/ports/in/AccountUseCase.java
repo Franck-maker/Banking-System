8daa0be460add24d7a07e200182c80a2281ae16f
Franck Abdiel <foziefranck@gmail.com>
2026-02-26 06:58:23 +0100

    feat(application) : define input port

---
+++ src/main/java/com/bank/banking_system/application/ports/in/AccountUseCase.java
@@ -0,0 +1,15 @@
+package com.bank.banking_system.application.ports.in;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import com.bank.banking_system.domain.model.Account; 
+
+public interface AccountUseCase {
+    //Returns the created account so the caller knows the new UUID
+    Account createAccount (String owner, BigDecimal initialBalance); 
+    // Returns the requested Account
+    Account getAccount(UUID accountId); 
+    //Performs a transfer between two accounts, ensuring atomicity and consistency
+    void transferMoney(UUID senderId, UUID receiver, BigDecimal amount); 
+}
