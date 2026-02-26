c6e632cb60d09a75848b9c3c15206d3142e977f2
Franck Abdiel <foziefranck@gmail.com>
2026-02-26 09:38:18 +0100

    feat(application) : define output port AccountRepositoryPort with Optional support

--- src/main/java/com/bank/banking_system/application/ports/in/AccountUseCase.java
+++ src/main/java/com/bank/banking_system/application/ports/in/AccountUseCase.java
@@ -5,6 +5,11 @@ import java.util.*;
 
 import com.bank.banking_system.domain.model.Account; 
 
+/**
+ * The AccountUseCase interface defines the operations related to bank accounts in the banking system.
+ * Tells the Controller what it can do
+ */
+
 public interface AccountUseCase {
     //Returns the created account so the caller knows the new UUID
     Account createAccount (String owner, BigDecimal initialBalance); 
