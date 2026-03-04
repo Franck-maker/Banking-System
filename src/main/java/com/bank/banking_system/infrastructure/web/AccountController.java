8575d4fc5e830266e39d2a32101916093c665aca
Franck Abdiel <foziefranck@gmail.com>
2026-03-04 14:29:33 +0100

    feat(persistence): adding persistence layer for the database

--- src/main/java/com/bank/banking_system/infrastructure/web/AccountController.java
+++ src/main/java/com/bank/banking_system/infrastructure/web/AccountController.java
@@ -1,14 +1,18 @@
 package com.bank.banking_system.infrastructure.web;
 import com.bank.banking_system.application.ports.in.AccountUseCase;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
+import java.util.UUID;
+
 import com.bank.banking_system.domain.model.Account;
 
 @RestController
@@ -25,7 +29,7 @@ public class AccountController {
     public ResponseEntity<AccountResponse> createAccount(@Valid @RequestBody CreateAccountRequest request){
 
         // We call the use case
-        Account createdAccount = accountUseCase.createAccount(request.owner(), request.initialBalance());
+        Account createdAccount = accountUseCase.createAccount(request.getOwner(), request.getInitialBalance());
         
         // Map the Domain Entity to a Response DTO
         AccountResponse response = new AccountResponse(
@@ -35,6 +39,20 @@ public class AccountController {
         );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponse> getAccount(@PathVariable UUID id){
+
+        Account account = accountUseCase.getAccount(id); 
+
+        AccountResponse response = new AccountResponse(
+             account.getAccountId(),
+             account.getOwnerName(), 
+             account.getBalance()
+            );
+        
+        return ResponseEntity.ok(response); 
     }
     
     
